3e8de660be463f646eb6cfca6cff89302a254e28
Gustav Larson <gustav.larson@gmail.com>
2009-05-03 22:45:00 +0200

    Added distutils setup.py to make installation simpler

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+version = '0.1'
+
+setup (name='Swedish Holidays',
+      version=version,
+      description='Useful functions to test if a date is a Swedish holiday, to generate lists of Swedish holidays, etc',
+      author='Gustav Larson',
+      author_email='gustav.larson@gmail.com',
+      license='MIT',
+      py_modules=['swedishholidays']
+)
